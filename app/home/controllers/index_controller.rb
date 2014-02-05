de2e7cb126da675275a0991f5f69b45cf18b8189
Corey <coreystout@gmail.com>
2014-02-05 18:51:45 -0500

    fix

--- app/home/controllers/index_controller.rb
+++ app/home/controllers/index_controller.rb
@@ -8,24 +8,20 @@ class IndexController < ModelController
   end
   
   def dom_ready
-    puts 'test'
-    %x{
-      var listen = $('.nav-link').click(function(){
-        console.log('clicked')
-
-      })
-    }
+    # %x{
+    #   var listen = $('.nav-link').click(function(){
+    #     console.log('clicked')
+    #   })
+    # }
     `$('body').data('loc', $('.nav-link.active').data('loc'))`
   end
   
   def dom_remove
     puts 'removed'
-    `listen.remove();`
   end
   
   def nav_click(i)
     %x{
-      return false;
       oldLoc = $('body').data('loc');
       $('body').data('loc', i);
       console.log(i, oldLoc)
