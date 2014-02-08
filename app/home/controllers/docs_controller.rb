9cfeb5de1e48e45adf5396920d7beee0884d9fb8
Corey <coreystout@gmail.com>
2014-02-08 00:32:28 -0500

    fix

--- app/home/controllers/docs_controller.rb
+++ app/home/controllers/docs_controller.rb
@@ -3,15 +3,21 @@ class DocsController < ModelController
 
   def initialize
     super
-
+    
   end
   
   def dom_ready
-    %x{
+    %x{    
       prettyPrint();
-      $('h1').attr('id', function(){
-        return $(this).text().toLowerCase();
+      $('h1, h2, h3').attr('id', function(){
+        return $(this).text().toLowerCase().replace(/[\*\^\'\!]/g, '').split(' ').join('-');
       });
+      
+      $('h1, h2, h3').attr('name', function(){
+        return $(this).text().toLowerCase().replace(/[\*\^\'\!]/g, '').split(' ').join('-');
+      });
+      
+      $('body').scrollspy({ target: '#spy-list' });
     }
   end
 
