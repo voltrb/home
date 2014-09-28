1e918b88a46257cab999b86c5eff8798569f53b0
Ryan Stout <ryanstout@gmail.com>
2014-09-27 18:07:39 -0600

    update docs and social buttons

--- bin/convert_readme.rb
+++ bin/convert_readme.rb
@@ -2,7 +2,7 @@ require 'github/markup'
 require 'open-uri'
 
 file = 'https://raw.github.com/voltrb/volt/master/Readme.md'
-destination = 'app/home/views/docs/readme.html'
+destination = 'app/main/views/docs/readme.html'
 content = open(file).read
 
 @doc = GitHub::Markup.render(file, content)
@@ -10,7 +10,7 @@ content = open(file).read
 @doc = @doc.gsub('}','{38.chr + "#125;"}')
 @doc = @doc.gsub('***************', '{"&#123;"}')
 
-@doc = @doc.gsub!(/\<pre(?: lang="(.+?)")?\>(.+?)\<\/pre\>/m) do 
+@doc = @doc.gsub!(/\<pre(?: lang="(.+?)")?\>(.+?)\<\/pre\>/m) do
   "<pre class='prettyprint lang-ruby'>#{$2}</pre>"
 end
 
