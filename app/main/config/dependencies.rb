9dbf5fbc98fc8b1bbbe4d7d5fe853bb10143b8a8
Ryan Stout <ryanstout@gmail.com>
2015-09-18 11:47:17 -0600

    move prettify into project

--- app/main/config/dependencies.rb
+++ app/main/config/dependencies.rb
@@ -4,7 +4,7 @@
 # bootstrap css framework
 component 'bootstrap'
 
-javascript_file 'https://google-code-prettify.googlecode.com/svn/loader/prettify.js'
+# javascript_file 'https://google-code-prettify.googlecode.com/svn/loader/prettify.js'
 
 # provides templates for login, signup, and logout
 component 'user_templates'
